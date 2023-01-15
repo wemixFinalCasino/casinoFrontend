0ec875094fc81b1187bfdddbcfd73c48c245bc0a
gomtak <tak6831@gmail.com>
2023-01-15 23:35:08 +0900

    Feat: Add get wemix balance

--- src/pages/MyWallet.jsx
+++ src/pages/MyWallet.jsx
@@ -1,15 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Web3 from "web3";
+import axios from "axios";
 
 const MyWallet = () => {
-    const accounts = async () => {
+    const [account, setAccount] = useState("");
+    const [balance, setBalance] = useState(0);
+
+    const getAccount = async () => {
         return await window.ethereum.request({ method: 'eth_requestAccounts' });
     } 
-    accounts().then((data)=>{
-        setAccount((prev) => prev = data)
-    });
-    const [account, setAccount] = useState(0);
-    const [balance, setBalance] = useState(0);
+    const getBalance = async (accounts) => {
+        return await window.ethereum.request({ method: 'eth_getBalance' , params: [ accounts, 'latest'] })
+    }
+    useEffect(() => {
+        getAccount().then((accounts)=>{ 
+            setAccount(accounts)
+        });
+    }, []);
+    useEffect(() => {
+        if(account !== ""){
+            axios.post('https://explorerapi.test.wemix.com/v1/accounts/balance',
+            {
+                addresses: account
+            },{
+                headers : {
+                    "api-key" : "1ba5e446edf1997f67b51bf9e60b3fbba6fa1bf84301115292805d7e24f43539",
+                    "Content-Type": "application/json"
+                }    
+            })
+            .then(function (response) {
+                const wemix = response.data.results.data[0].balance
+                setBalance(wemix / Math.pow(10,18))
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        }
+    }, [account]);
+    // getBalance(accounts).then((balance)=>{
+    //     console.log(balance);
+    //     // const wemix = parseInt(balance, 16)
+    //     // setBalance(wemix)    
+    // })
     return (
         <>
         <h5>{account}<br></br>{balance} WIMIX</h5>
